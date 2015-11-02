23670a7f8aad37f4e987f1b3856c8bd634e1f5db
Carl Pearson <cap10@ufl.edu>
2015-11-01 23:15:44 -0500

    code for hard part of cycle analysis

--- utils.R
+++ utils.R
@@ -25,17 +25,19 @@ krings <- lapply(ring.sizes, graph.ring)
 
 cores <- min(detectCores()-1, length(ring.sizes))
 
-score <- function(s, e, res) {
+score <- function(s, e, res, t) {
   prs <- pairs(s*24*60*60, e*24*60*60, res)
   remap_ids <- data.table(user_id = prs[,unique(c(userA,userB))])[,new_user_id := .I, by=user_id]
   relabelled <- data.table(userA=remap_ids[user_id == prs$userA, new_user_id], userB=remap_ids[user_id == prs$userB, new_user_id])
-  el <- as.matrix(relabelled)
+  el <- t(as.matrix(relabelled))
   dim(el) <- NULL
   g <- graph(el, directed = F)
   mapply(
     function(cyc, sz) if (length(cyc) != 0) {
-      src <- matrix(unlist(cyc), byrow = T, ncol=sz)
-      data.table(unique(t(apply(src, 1, sort))), time=s)
+      src <- unique(t(apply(matrix(unlist(cyc), byrow = T, ncol=sz), 1, sort)))
+      lsrc <- lapply(1:(dim(src)[2]), function(col) remap_ids[new_user_id == src[, col], user_id])
+      lsrc$time=t
+      do.call(data.table, lsrc)
     } else {
       data.table(matrix(1, ncol=sz), time=0)[0]
     },
@@ -44,20 +46,64 @@ score <- function(s, e, res) {
   )
 }
 
-processList <- function(run, startDay=100, window = 7, endDay=startDay + 100*window) {
+processList <- function(run, startDay=100, window = 14, endDay=startDay + 100*window) {
   cat("starting week, day ", startDay,"\n")
   res <- setkey(rbind(background, run[,list(userA, userB, start=login, end=logout, reason)]), start)[, start := start - firstday*60*60*24][, end := end - firstday*60*60*24]
   
   ## TODO add this pairs script
   res <- res[,c(list(swap=userA>userB), .SD)][,list(userA=ifelse(swap, userB, userA), userB=ifelse(swap, userA, userB), start, end, reason)]
   ## write starting scores
-  
-  results <- score(startDay, startDay+window, res)
+  i <- 0
+  results <- score(startDay, startDay+window, res, i)
   
   for (s in seq(startDay+window, endDay, by=window)) {
     cat("starting week, day ", s,"\n")
-    appresults <- score(s, s+window, res)
+    i <- i+1
+    appresults <- score(s, s+window, res, i)
     for (j in 1:length(ring.sizes)) results[[j]] <- rbind(results[[j]], appresults[[j]])
   }
   results
 }
+
+testAnalysis <- processList(run, window=14, endDay = 100+14*52)
+
+## base score for each pair == pairing count in interval in excess of 1
+## each pairing appearance garners score of 1/cycle length
+allcolpairs <- lapply(ring.sizes, function(n) combn(paste("V",1:n,sep=""), 2))
+
+longitudinalScore <- function(processed) {
+  setkey(rbindlist(
+      mcmapply(function(kres, colpairs) {
+      k <- dim(kres)[2] - 1
+      setnames(rbindlist(apply(colpairs, 2, function(col) kres[,.N,by=c(col, "time")]))[,list(N=sum(N), k=k),keyby=list(time,V1,V2)], c("V1","V2"), c("userA","userB"))
+    }, processed, allcolpairs, SIMPLIFY = F)
+  ), time, k)
+}
+
+tempThing <- longitudinalScore(testAnalysis)[,
+  list(swap=userA>userB, score=N/k), by=list(time,userA, userB)
+][,
+  list(userA=ifelse(swap,userB,userA), userB=ifelse(swap,userA,userB), score, time)
+][,
+  list(score=sum(score)), keyby=list(userA,userB,time)
+]
+
+detectByScores <- function(dt, discount=.9) {
+  temp <- rbindlist(
+    list(dt[,sum(N/k),keyby=list(time, userA)],
+    dt[,sum(N/k),keyby=list(time, userB)])
+  )
+  temp <- temp[,list(score = sum(V1)),keyby=list(time,userA)]
+  setkey(setnames(temp, "userA", "user"), user, time)
+  users <- sort(unique(temp$userA)); tmax <- temp[,max(time)]
+  ref <- data.table(user=rep(users, each=tmax+1), time=rep(0:tmax, times=length(users)), key=c("user","time"))
+  expand <- temp[ref]
+  expand[is.na(score), score := 0]
+  expand[, list(time, score = Reduce(function(left, right) { left*discount + right }, score, accumulate=T)), by=user]
+  # temp[,list(time, score = cumsum(score)),keyby=list(userA)]
+}
+
+ggplot(thing) + theme_bw() +
+  aes(x=time, y=score, color=utype, alpha=utype, group=user) + geom_line() +
+  scale_color_manual(values = c(covert="red", background="black")) +
+  scale_alpha_manual(values = c(covert=1, background=.1))
